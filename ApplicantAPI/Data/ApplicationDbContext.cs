9943f85878f88f1f82d4b8392426de6d909264b5
din0115 <prashantshrestha027@gmail.com>
2024-11-19 18:46:28 +0545

    Creare ApplicationDbContext

---
+++ ApplicantAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApplicantAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicantAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Applicant> Applicants { get; set; }
+
+    }
+}
---
+++ ApplicantAPI/Models/Entities/Applicant.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApplicantAPI.Models.Entities
+{
+    public class Applicant
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public required string FirstName { get; set; }
+        [MaxLength(50)]
+        public string MiddleName { get; set; }
+        [MaxLength(50)]
+        public required string LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public required string Email { get; set; }
+        public required string Comment { get; set; }
+        public string? LinkedIn { get; set; }
+        public string? GitHub { get; set; }
+    }
+}
