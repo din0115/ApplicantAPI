62a6ac98e329decbcdc6f78bded341c027f1cc4d
din0115 <prashantshrestha027@gmail.com>
2024-11-19 19:30:18 +0545

    Update the Applicant Data

--- ApplicantAPI/Controllers/ApplicantController.cs
+++ ApplicantAPI/Controllers/ApplicantController.cs
@@ -22,6 +22,17 @@ namespace ApplicantAPI.Controllers
             var allApplicant = dbContext.Applicants.ToList();
             return Ok(allApplicant);
         }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetApplicantById(int id)
+        {
+            var applicant = dbContext.Applicants.Find(id);
+            if (applicant == null) 
+            {
+                return NotFound();
+            }
+            return Ok(applicant);
+        }
         [HttpPost]
         public IActionResult AddApplicant(AddApplicantDto addApplicantDto)
         {
@@ -41,5 +52,28 @@ namespace ApplicantAPI.Controllers
             dbContext.SaveChanges();
             return Ok("Your Data has been added successfull!");
         }
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateApplicant(int id, AddApplicantDto addApplicantDto)
+        {
+            var applicant = dbContext.Applicants.Find(id);
+            if (applicant == null)
+            {
+                return NotFound("This Id Doesn't Exist");
+            }
+
+
+            applicant.FirstName = addApplicantDto.FirstName;
+            applicant.MiddleName = addApplicantDto.MiddleName;
+            applicant.LastName = addApplicantDto.LastName;
+            applicant.Email = addApplicantDto.Email;
+            applicant.PhoneNumber = addApplicantDto.PhoneNumber;
+            applicant.Comment = addApplicantDto.Comment;
+            applicant.LinkedIn = addApplicantDto.LinkedIn;
+            applicant.GitHub = addApplicantDto.GitHub;
+            dbContext.SaveChanges();
+            return Ok("Your Data has been updated successfull!");
+        }
+
     }
 }
